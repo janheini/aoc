4d5c282befabc6f16c3b7a5ac283adaa692854b2
Jan Heinemeyer <janheinemeyer99@gmail.com>
2024-07-25 14:56:18 +0200

    day3 part2

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn is_valid(grid: &[Vec<char>], x: usize, y: usize) -> bool {
     const SYMBOLS: [char; 10] = ['@', '#', '$', '%', '&', '*', '-', '=', '+', '/'];
 
@@ -18,8 +20,60 @@ fn is_valid(grid: &[Vec<char>], x: usize, y: usize) -> bool {
         .any(|&c| c.is_some() && SYMBOLS.contains(c.unwrap()));
 }
 
-pub fn part1(input: &str) -> u32 {
-    let mut result = 0;
+#[derive(Eq, Hash, PartialEq, Debug)]
+struct Coordinate {
+    x: usize,
+    y: usize,
+}
+
+fn gear(
+    grid: &[Vec<char>],
+    position: &Coordinate,
+    value: u32,
+    map: &mut HashMap<Coordinate, u32>,
+) -> u32 {
+    /* Checks for gear on position
+     *  stores value in map for first part of gear
+     *  returns gear value or 0 */
+    let size = value.to_string().len();
+
+    for i in 1..=size {
+        let pos = Coordinate {
+            x: position.x.saturating_sub(i),
+            y: position.y,
+        };
+
+        for hor in 0..=2 {
+            for ver in 0..=2 {
+                let x = (pos.x + 1).saturating_sub(hor);
+                let y = (pos.y + 1).saturating_sub(ver);
+                let gear = grid.get(y).and_then(|row| row.get(x));
+                let coord = Coordinate { x, y };
+
+                if let Some(&'*') = gear {
+                    match map.remove(&coord) {
+                        Some(val) => {
+                            // gear found
+                            return value * val;
+                        }
+                        None => {
+                            // first part of a gear, save in map
+                            map.insert(coord, value);
+                            return 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    0
+}
+
+pub fn day3(input: &str) -> (u32, u32) {
+    let mut part1 = 0;
+    let mut part2 = 0;
+    let mut map: HashMap<Coordinate, u32> = HashMap::new();
 
     let mut grid: Vec<Vec<char>> = vec![];
 
@@ -27,22 +81,24 @@ pub fn part1(input: &str) -> u32 {
         grid.push(line.chars().collect());
     }
 
-    for (x, row) in grid.iter().enumerate() {
+    for (y, row) in grid.iter().enumerate() {
         let mut value = 0;
         let mut valid = false;
 
-        for (y, c) in row.iter().enumerate() {
+        for (x, c) in row.iter().enumerate() {
             if let Some(c) = c.to_digit(10) {
                 value = (value * 10) + c;
-                println!("value {value}");
                 if !valid {
-                    valid = is_valid(&grid, x, y);
+                    valid = is_valid(&grid, y, x);
                 }
             } else {
                 // number ended, add if valid
                 if valid {
-                    result += value;
+                    part1 += value;
+                    // println!("{x}, {y}, {c}");
+                    part2 += gear(&grid, &Coordinate { x, y }, value, &mut map);
                 }
+
                 value = 0;
                 valid = false;
             }
@@ -50,15 +106,18 @@ pub fn part1(input: &str) -> u32 {
 
         // line ended, add if number at end of line was valid
         if valid {
-            result += value;
+            part1 += value;
+            part2 += gear(&grid, &Coordinate { x: row.len(), y }, value, &mut map);
         }
     }
 
-    result
+    (part1, part2)
 }
 
 #[cfg(test)]
 mod tests {
+    use core::panic;
+
     use super::super::*;
     use super::*;
 
@@ -77,7 +136,7 @@ mod tests {
     #[test]
     fn day3_example() {
         println!("{EXAMPLE}");
-        assert_eq!(part1(EXAMPLE), 4361);
+        assert_eq!(day3(EXAMPLE), (4361, 467835));
     }
 
     #[test]
@@ -98,7 +157,21 @@ mod tests {
         let input = read_input(day);
 
         if let Some(solution) = read_solution(day, &part) {
-            assert_eq!(part1(&input), solution, "day{day} part{part}");
+            assert_eq!(day3(&input).0, solution, "day{day} part{part}");
+        }
+    }
+
+    #[test]
+    fn day3_part2() {
+        let day = 3;
+        let part = Part::Two;
+        let input = read_input(day);
+
+        if let Some(solution) = read_solution(day, &part) {
+            assert_eq!(day3(&input).1, solution, "day{day} part{part}");
+        } else {
+            println!("Proposed Solution: {}", day3(&input).1);
+            panic!("No solution yet.");
         }
     }
 }
